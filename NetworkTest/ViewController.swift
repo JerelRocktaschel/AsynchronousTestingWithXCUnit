0acdc5a11ea0c0202807fae83fb45f9068a8e978
Jerel Rocktaschel <jerel.rocktaschel@gmail.com>
2021-02-11 20:16:34 -0500

    Initial Commit

---
+++ NetworkTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NetworkTest
+//
+//  Created by Jerel Rocktaschel rMBP on 2/11/21.
+//  Copyright © 2021 HighScoreApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
